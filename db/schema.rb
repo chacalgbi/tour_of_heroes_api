f096e986060af3526209b75429f672e224cb35ca
Thomé Lucas A. Oliveira <chacalgbi@hotmail.com>
2022-04-18 07:43:35 -0300

    Add coluna token em Heroes

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_11_120811) do
+ActiveRecord::Schema.define(version: 2022_04_18_103835) do
 
   create_table "heroes", force: :cascade do |t|
     t.string "name", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "token", null: false
+    t.index ["token"], name: "index_heroes_on_token"
   end
 
 end
